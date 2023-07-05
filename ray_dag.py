f881d89dd4a67c895154c3bf1dc9fe81dd87e200
arturodonate <45829428+arturodonate@users.noreply.github.com>
2023-07-05 11:57:44 -0400

    Update ray_dag.py

--- ray_dag.py
+++ ray_dag.py
@@ -3,28 +3,32 @@ from ray.serve.drivers import DAGDriver
 from ray.serve.deployment_graph import InputNode
 from starlette.requests import Request
 
-# Unpack http request
+# Unpack request
 @serve.deployment
 async def unpack_request(http_request: Request) -> float:
     return await http_request.json()
 
-# Node 1 of the DAG
+# Nodes in the graph
 @serve.deployment
 async def func1(number: float) -> float:
-    return number * 10
+    return number + 5
 
-# Node 2 of the DAG
 @serve.deployment
 async def func2(number: float) -> float:
-    return number + 2
+    return number * 3
 
-# Bind functions
+@serve.deployment
+async def func3(number: float) -> float:
+    return number - 4
+
 func1_node = func1.bind()
 func2_node = func2.bind()
+func3_node = func3.bind()
 
 with InputNode() as http_request:
-    request_number = unpack_request.bind(http_request)
-    func1_output = func1.bind(request_number)
+    unpack_output = unpack_request.bind(http_request)
+    func1_output = func1.bind(unpack_output)
     func2_output = func2.bind(func1_output)
+    func3_output = func3.bind(func2_output)
 
-graph = DAGDriver.options(route_prefix="/inference").bind(func2_output)
+graph = DAGDriver.options(route_prefix="/inference").bind(func3_output)
